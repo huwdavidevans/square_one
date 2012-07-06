334baf6a1d399d7d68fa0b33fe982bf3960fc74e
Huw Evans <huwdavidevans@gmail.com>
2012-07-06 15:02:49 +0100

    Created  project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquareOne::Application.config.secret_token = 'f7072981bd68051e7b356323463a6365d3004a71dd632ef5c72f7749d6492eafeb3f88ecf31f9a38534987c696ffaf4100c90f70be48a2e77ad4fc35c26ef5e7'
