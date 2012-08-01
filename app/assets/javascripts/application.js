bd60dd19259421a0678985790eb56159ccea5a92
Huw Evans <huwdavidevans@gmail.com>
2012-08-01 11:09:11 +0100

    Tidied up javascript for Task Piecharts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,68 +15,77 @@
 //= require_tree .
 
 
+function convertAngle(amt, degrees){
+	degrees = typeof degrees !== 'undefined' ? degrees : true;
+	var output = 0;	
+	if (degrees){
+		//output radians
+		output = amt * (Math.PI / 180);
+	} else{
+		//output degrees
+		output = amt * (180 / Math.PI);
+	}	
+	return output;
+}
 
-function drawCircleIn(divID, colour) {
-	var myDiv = $('#'+divID)[0];
+
+function angleObject(angle){
+	var angleType = String(angle).charAt(angle.length-1);
+	angle = Number(angle.split(angleType)[0]);
+	switch (angleType){		
+		case '%' :
+			this.percent = angle;
+			this.degrees = 360 * angle;
+			this.radians = convertAngle(this.degrees);
+		break;
+		case 'd' :
+			this.degrees = angle;
+			this.percent = this.degrees / 360;
+			this.radians = convertAngle(this.degrees);
+		break;		
+		case 'r' :
+			this.radians = angle;
+			this.degrees = convertAngle(this.degrees, false);
+			this.percent = this.degrees / 360;
+		break;
+	}	
+}
+
+
+function drawPi(myDiv, start, amount, radiusPadding, colour){	
+	colour = typeof colour !== 'undefined' ? colour : "#444";
+	var ctx = myDiv.getContext("2d");
+	var _s = new angleObject(start);
+	var _e = new angleObject(amount);
+	var ninetyDeg = convertAngle(90);
 	var _w = myDiv.width;
 	var _h = myDiv.height;
-	var ctx = myDiv.getContext("2d");
-
-	var radius; (_w > _h) ? radius = (_h - 4) / 2 : radius = (_w - 4) / 2; 
-	colour == null ? ctx.fillStyle = "#444" : ctx.fillStyle = colour;
+	var radius; (_w > _h) ? radius = (_h - radiusPadding) / 2 : radius = (_w - radiusPadding) / 2;
+	ctx.fillStyle = colour;
 	ctx.beginPath();
-	//arc(x, y, radius, startAngle, endAngle, anticlockwise);
-	ctx.arc(_w / 2, _h / 2, radius, 0, Math.PI * 2, true);
+	ctx.arc(_w / 2, _h / 2, radius, _s.radians-ninetyDeg, _s.radians+_e.radians-ninetyDeg, false);
+	ctx.lineTo(_w / 2, _h / 2);
 	ctx.closePath();
 	ctx.fill();
 }
 
-
-
-function drawPieIn(divID, slicePercent, colour) {
+function drawTaskPie(divID, slicePercent) {	
 	var colArray = ["#00df00", "#5fdf00", "#9fdf00", "#dfdf00", "#ffdf00", "#ffbf00", "#ff9f00", "#ff7f00", "#ff3f00", "#ff0000"]
-	
-	drawCircleIn(divID, "#aeaeae");
-	
+	colour = colArray[parseInt(slicePercent * 9, 10)];	
+	var myDiv = $('#'+divID)[0];
+	//draw background circle
+	drawPi(myDiv, '0%', '1%', 4, "#aeaeae");
+	//draw time pie
 	if(slicePercent > 0) {
-
-		var myDiv = $('#'+divID)[0];
-		var _w = myDiv.width;
-		var _h = myDiv.height;
-		var ctx = myDiv.getContext("2d");
-
-		var radius; (_w > _h) ? radius = (_h - 8) / 2 : radius = (_w - 8) / 2;
-
-		var startAngle = (Math.PI / -2);
-		var endAngle = (Math.PI * 2) * Number(slicePercent) ; 
-		
-		colour == null ? ctx.fillStyle = colArray[parseInt(slicePercent * 9 ,10)] : ctx.fillStyle = colour;
-		console.log(ctx.fillStyle)
-		
-		ctx.beginPath();
-		//arc(x, y, radius, startAngle, endAngle, anticlockwise);
-		ctx.arc(_w / 2, _h / 2, radius, startAngle, startAngle+endAngle, false);
-		ctx.lineTo(_w / 2, _h / 2);
-		ctx.closePath();
-		ctx.fill();
-	}
-	
-	
-		if(slicePercent > 1) {
-
-		var myDiv = $('#'+divID)[0];
-		var _w = myDiv.width;
-		var _h = myDiv.height;
-		var ctx = myDiv.getContext("2d");
-		var radius; (_w > _h) ? radius = (_h - 8) / 2 : radius = (_w - 8) / 2;
-		var startAngle = 0;
-		var endAngle = (Math.PI * 2) ; 
-		ctx.fillStyle = colArray[colArray.length-1]
-		ctx.beginPath();
-		//arc(x, y, radius, startAngle, endAngle, anticlockwise);
-		ctx.arc(_w / 2, _h / 2, radius, startAngle, endAngle, false);
-		ctx.lineTo(_w / 2, _h / 2);
-		ctx.closePath();
-		ctx.fill();
-	}
+		drawPi(myDiv, '0d', slicePercent+'%', 8, colour);
+	} else if(slicePercent > 1) {
+		drawPi(myDiv, '0%', '1%', 8, colArray[colArray.length-1]);
+	}	
 }
+
+
+
+
+
+
+
